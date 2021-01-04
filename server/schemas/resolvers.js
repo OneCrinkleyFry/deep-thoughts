a543231a0f92de1da4f906370bcb0a9e64b65036
JoshMiller5994 <talk2joshbgosh@gmail.com>
2021-01-03 19:56:03 -0700

    added the jwt

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,7 @@
 const { User, Thought } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const { gql } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
    Query: {
@@ -30,8 +32,9 @@ const resolvers = {
    Mutation: {
       addUser: async (parent, args) => {
          const user = await User.create(args);
+         const token = signToken(user);
 
-         return user;
+         return { token, user };
       },
       login: async (parent, { email, password }) => {
          const user = await User.findOne({ email });
@@ -46,7 +49,9 @@ const resolvers = {
             throw new AuthenticationError('Incorrect credentials');
          }
 
-         return user;
+         const token = signToken(user);
+
+         return { token, user };
       }
    }
 };
