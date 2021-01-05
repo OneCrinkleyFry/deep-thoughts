39fc6d5d302e873b718bf9a344c5fdc889eb8b9a
JoshMiller5994 <talk2joshbgosh@gmail.com>
2021-01-04 22:47:49 -0700

    displays friends

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,6 +4,8 @@ import ThoughtList from '../components/ThoughtList';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_USER } from '../utils/queries';
 
+import FriendList from '../components/FriendsList';
+
 const Profile = () => {
   const { username: userParam } = useParams();
 
@@ -28,6 +30,13 @@ const Profile = () => {
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
           <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
+        </div>
+        <div className="col-12 col-lg-3 mb-3">
+          <FriendList
+            username={user.username}
+            friendCount={user.friendCount}
+            friends={user.friends}
+          />
         </div>
       </div>
     </div>
